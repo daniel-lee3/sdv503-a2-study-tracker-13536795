8cb25772000a1798ae1b875929dd3b3838b81dcd
daniel-lee3 <daniel-lee3@live.nmit.ac.nz>
2026-05-25 13:21:05 +1200

    JSON file saving

--- study-tracker.js
+++ study-tracker.js
@@ -2,21 +2,23 @@ const { stdin, stdout } = require('node:process');
 const readline = require('node:readline');
 const fs = require('node:fs');
 
-const studyPeriods = {
-    // For the data structure I decided the index will be the topic the user inputs
-    // The value will be an array showcasing a history allowing for filtering over time periods
-    // I decided not to include a totalMinutes as it can be calculated by adding all-time history
-    // The history will be saved as JSON where it can be read
-    // -------- STRUCTURE --------
-    // topic: [
-    //   {
-    //     timeRecorded: timestamp
-    //     minutes: number
-    //   }
-    // ]
-}
+const filePath = 'recorded-studies.json'
 
 async function main() {
+    const studyPeriods = await loadJSON(filePath, {
+        // For the data structure I decided the index will be the topic the user inputs
+        // The value will be an array showcasing a history allowing for filtering over time periods
+        // I decided not to include a totalMinutes as it can be calculated by adding all-time history
+        // The history will be saved as JSON where it can be read
+        // -------- STRUCTURE --------
+        // topic: [
+        //   {
+        //     timeRecorded: timestamp
+        //     minutes: number
+        //   }
+        // ]
+    })
+
     const rl = readline.promises.createInterface({
         input: stdin,
         output: stdout
@@ -25,9 +27,22 @@ async function main() {
     try {
         const topicInput = await rl.question('What is the topic? ');
         const minutesInput = await rl.question('How long did you study for? (in minutes) ');
+        const overviewInput = await rl.question('Would you like an overview? (y/n) ')
+        const overviewAcceptedInputs = {
+            yes: [
+                'yes',
+                'y'
+            ],
+            no: [
+                'no',
+                'n'
+            ]
+        }
+
 
         const topic = topicInput.trim();
         const minutes = Number(minutesInput);
+        let overview = '';
         
         const issues = [];
         if (topic.length === 0) {
@@ -38,12 +53,19 @@ async function main() {
         } else if (minutes <= 0) {
             issues.push('Study time must be longer than 0 minutes');
         }
+        if (overviewAcceptedInputs.yes.includes(overviewInput.toLowerCase())) {
+            overview = true
+        } else if (overviewAcceptedInputs.no.includes(overviewInput.toLowerCase())) {
+            overview = false
+        } else {
+            issues.push('Overview must be either "y" or "n"');
+        }
         if (issues.length > 0) {
             throw Error(issues.join(', '));
         }
 
         if (topicInput !== topic) {
-            console.log(`Topic storing as ${topic}`);
+            console.log(`Topic storing as "${topic}"`);
         }
 
         if (studyPeriods[topic] == undefined) {
@@ -53,11 +75,40 @@ async function main() {
             timeRecorded: Date.now(),
             minutes: minutes
         })
+
+        if (overview) {
+            let overview = [];
+            for (let topic in studyPeriods) {
+                const history = studyPeriods[topic]
+                overview.push(`---------- ${topic} ----------`)
+                history.map((entry, index) => {overview.push(`${new Date(entry.timeRecorded).toDateString()}: ${entry.minutes} minutes`)})
+                overview.push(`--------------------`)
+                overview.push(`Total time: ${history.reduce((sum, entry) => sum + entry.minutes, 0)} minutes\n`)
+            }
+            console.log(overview.join('\n'))
+        }
     } catch(Error) {
-        console.log(`Something went wrong.\n    %c${Error}`, 'color: red;')
+        console.log(`Something went wrong.\n    %c${Error}`, 'color: red;');
     } finally {
         rl.close();
+
+        writeJSON(filePath, studyPeriods);
     }
+}
+
+async function loadJSON(filePath, defaultValue) {
+    try {
+        const json = await fs.promises.readFile(filePath, 'utf8');
+        return JSON.parse(json);
+    } catch(err) {
+        return defaultValue;
+    }
+}
+
+async function writeJSON(filePath, data) {
+    const jsonData = JSON.stringify(data, null, 2);
+
+    fs.promises.writeFile(filePath, jsonData)
 }
 
 main()
