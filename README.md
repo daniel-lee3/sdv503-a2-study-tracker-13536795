56dc7a47f81d0dea8b051eff99793c9584ef1b25
daniel-lee3 <daniel-lee3@live.nmit.ac.nz>
2026-05-26 09:48:28 +1200

    Added seperate tasks + Partial history viewer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sdv503-a2-study-tracker-13536795
 Study tracker application written in JavaScript, for assignment 2 of SDV503, run with node.js
+
+## Data Structure
+All data is stored inside a .json file named **'recorded-studies.json'**.
+Data is stored as the following
+```json
+[
+    {
+        timeRecorded: timestamp
+        minutes: number
+    }
+]
+```
--- study-tracker.js
+++ study-tracker.js
@@ -5,44 +5,54 @@ const fs = require('node:fs');
 const filePath = 'recorded-studies.json'
 
 async function main() {
-    const studyPeriods = await loadJSON(filePath, {
-        // For the data structure I decided the index will be the topic the user inputs
-        // The value will be an array showcasing a history allowing for filtering over time periods
-        // I decided not to include a totalMinutes as it can be calculated by adding all-time history
-        // The history will be saved as JSON where it can be read
-        // -------- STRUCTURE --------
-        // topic: [
-        //   {
-        //     timeRecorded: timestamp
-        //     minutes: number
-        //   }
-        // ]
-    })
+    const rl = readline.promises.createInterface({
+        input: stdin,
+        output: stdout
+    });
+    const studyPeriods = await loadJSON(filePath, {});
 
+    console.log('---------- Study Tracker ----------');
+    console.log('   1. Add Study Session');
+    console.log('   2. View full history');
+    console.log('   3. View history');
+    console.log();
+    const input = await rl.question('Enter input: ');
+    rl.close();
+
+    switch(input) {
+        case '1':
+            const newData = await addStudyPeriod();
+            studyPeriods[newData.topic].push({
+                timeRecorded: newData.timeRecorded,
+                minutes: newData.minutes
+            });
+            writeJSON(filePath, studyPeriods);
+            break;
+        case '2':
+            const overviewString = await getOverview(studyPeriods);
+            console.log(overviewString);
+            break;
+        case '3':
+            const partialOverviewString = await getPartialOverview(studyPeriods);
+            console.log(partialOverviewString);
+            break;
+    }
+}
+
+async function addStudyPeriod() {
     const rl = readline.promises.createInterface({
         input: stdin,
         output: stdout
     });
 
+    let data;
+
     try {
         const topicInput = await rl.question('What is the topic? ');
         const minutesInput = await rl.question('How long did you study for? (in minutes) ');
-        const overviewInput = await rl.question('Would you like an overview? (y/n) ')
-        const overviewAcceptedInputs = {
-            yes: [
-                'yes',
-                'y'
-            ],
-            no: [
-                'no',
-                'n'
-            ]
-        }
-
 
         const topic = topicInput.trim();
         const minutes = Number(minutesInput);
-        let overview = '';
         
         const issues = [];
         if (topic.length === 0) {
@@ -52,13 +62,6 @@ async function main() {
             issues.push('Study time must be a number');
         } else if (minutes <= 0) {
             issues.push('Study time must be longer than 0 minutes');
-        }
-        if (overviewAcceptedInputs.yes.includes(overviewInput.toLowerCase())) {
-            overview = true
-        } else if (overviewAcceptedInputs.no.includes(overviewInput.toLowerCase())) {
-            overview = false
-        } else {
-            issues.push('Overview must be either "y" or "n"');
         }
         if (issues.length > 0) {
             throw Error(issues.join(', '));
@@ -71,29 +74,44 @@ async function main() {
         if (studyPeriods[topic] == undefined) {
             studyPeriods[topic] = [];
         }
-        studyPeriods[topic].push({
+        data = {
+            topic: topic,
             timeRecorded: Date.now(),
             minutes: minutes
-        })
-
-        if (overview) {
-            let overview = [];
-            for (let topic in studyPeriods) {
-                const history = studyPeriods[topic]
-                overview.push(`---------- ${topic} ----------`)
-                history.map((entry, index) => {overview.push(`${new Date(entry.timeRecorded).toDateString()}: ${entry.minutes} minutes`)})
-                overview.push(`--------------------`)
-                overview.push(`Total time: ${history.reduce((sum, entry) => sum + entry.minutes, 0)} minutes\n`)
-            }
-            console.log(overview.join('\n'))
         }
     } catch(Error) {
         console.log(`Something went wrong.\n    %c${Error}`, 'color: red;');
     } finally {
         rl.close();
-
-        writeJSON(filePath, studyPeriods);
     }
+
+    return data
+}
+
+async function getPartialOverview(overviewInfo) {
+    const rl = readline.promises.createInterface({
+        input: stdin,
+        output: stdout
+    });
+
+    const days = await rl.question("How many days of history would you like to see? ");
+    rl.close();
+    const filterCallback = (value) => value.timeRecorded >= Date.now()-(86400 * days * 1000);
+    return await getOverview(overviewInfo, filterCallback);
+}
+
+async function getOverview(overviewInfo, filterCallback = () => true) {
+    let overview = [];
+    for (let topic in overviewInfo) {
+        const history = overviewInfo[topic].filter(filterCallback)
+        overview.push(`---------- ${topic} ----------`)
+        history.map((entry, index) => {
+            overview.push(`${new Date(entry.timeRecorded).toDateString()}: ${entry.minutes} minutes`)
+        })
+        overview.push(`--------------------`)
+        overview.push(`Total time: ${history.reduce((sum, entry) => sum + entry.minutes, 0)} minutes\n`)
+    }
+    return overview.join('\n')
 }
 
 async function loadJSON(filePath, defaultValue) {
